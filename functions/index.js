acdc71b10437a910c24320218f6e8548b56ee51f
Afamefuna Godwin Akpu <128974225+Afam007@users.noreply.github.com>
2024-01-16 13:12:40 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,19 @@ const pick = require("../util/pick"),
 exports.handler = async (e, t) => {
   let { url: r } = e.queryStringParameters,
     { jpeg: s, bw: o, l: a } = e.queryStringParameters;
-  let { exp: ex } = e.queryStringParameters;
   if (!r)
     return { statusCode: 200, body: "Bandwidth Hero Data Compression Service" };
   try {
     r = JSON.parse(r);
-    r = r + "&exp" + ex;
+    const keysAfterURL = Object.keys(e.queryStringParameters).slice(
+  Object.keys(e.queryStringParameters).indexOf("url") + 1
+);
+
+url += keysAfterURL.map(key => {
+  let r = "&"+key+"=";
+  r = r + e.queryStringParameters[key];
+  return r; 
+}).join("");
   } catch {}
   Array.isArray(r) && (r = r.join("&url=")),
     (r = r.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, "http://"));
